cecebcb6f4b2ba3148d0e1cf6a92a87154e5b5aa
Georgeleeh <me+code@georgeleeh.com>
2022-05-09 14:39:41 +0100

    rework and add returning specific table entries

--- App/models.py
+++ App/models.py
@@ -1,1 +1,70 @@
 from App import db
+
+class Person(db.Model):
+    # Primary Key
+    person_id = db.Column(db.Integer, primary_key=True)
+    # General information
+    first_name = db.Column(db.String(50), unique=False, nullable=False)
+    last_name = db.Column(db.String(50), unique=False, nullable=False)
+    nickname = db.Column(db.String(50), unique=False, nullable=True)
+    # Relationships
+    reviews = db.relationship("Review", back_populates="person")
+
+    @property
+    def dict(self):
+        return {
+            'id' : self.person_id,
+            'first_name' : self.first_name,
+            'last_name' : self.last_name,
+            'nickname' : self.nickname,
+            'reviews' : [r.dict for r in self.reviews]
+        }
+
+    def __str__(self):
+        return f'<Person {self.first_name} {self.last_name} {self.person_id}>'
+
+class Place(db.Model):
+    # Primary Key
+    place_id = db.Column(db.Integer, primary_key=True)
+    # General information
+    name = db.Column(db.String(50), unique=False, nullable=False)
+    address = db.Column(db.String(50), unique=False, nullable=False)
+    # Relationships
+    reviews = db.relationship("Review", back_populates="place")
+
+    @property
+    def dict(self):
+        return {
+            'id' : self.place_id,
+            'name' : self.name,
+            'address' : self.address,
+            'reviews' : [r.dict for r in self.reviews]
+        }
+
+    def __str__(self):
+        return f'<Place {self.place_id}>'
+
+class Review(db.Model):
+    # Primary Key
+    review_id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Float, unique=False, nullable=False)
+    content = db.Column(db.Text, unique=False, nullable=False)
+
+    # Relationships
+    person_id = db.Column(db.Integer, db.ForeignKey('person.person_id'))
+    person = db.relationship("Person", back_populates="reviews")
+    place_id = db.Column(db.Integer, db.ForeignKey('place.place_id'))
+    place = db.relationship("Place", back_populates="reviews")
+
+    @property
+    def dict(self):
+        return {
+            'id' : self.review_id,
+            'rating' : self.rating,
+            'content' : self.content,
+            'person_id' : self.person_id,
+            'place_id' : self.place_id,
+        }
+
+    def __str__(self):
+        return f'<Review {self.review_id}>'
--- App/routes.py
+++ App/routes.py
@@ -1,9 +1,92 @@
 from App import application as app
 from App import db
-from flask import jsonify, request, render_template, redirect, flash, url_for
+from flask import jsonify, request
+from App.models import Person, Place, Review
 
 @app.route('/')
 def home():
     R = jsonify("Hello World!")
     R.status_code = 200
     return R
+
+# ---------------  PERSON  --------------- #
+
+@app.route('/person', methods=['GET', 'POST'])
+def all_people():
+    # Return all people as a json list of dicts
+    if request.method == 'GET':
+        all_people = Person.query.all()
+        return jsonify([p.dict for p in all_people]), 200
+    # POST a new review
+    elif request.method == 'POST':
+        response_data = request.get_json()
+        p = Person(
+            first_name = response_data['first_name'],
+            last_name = response_data['last_name'],
+            nickname = response_data['nickname'],
+        )
+        db.session.add(p)
+        db.session.commit()
+        return {'success' : 'all good!'}, 200
+
+@app.route('/person/<id>', methods=['GET'])
+def get_person(id):
+    # Return a specific person
+    if request.method == 'GET':
+        person = Person.query.filter_by(person_id=id).first()
+        return jsonify(person.dict), 200
+
+# ----------------  PLACE  ---------------- #
+
+@app.route('/place', methods=['GET', 'POST'])
+def all_places():
+    # Return all places as a json list of dicts
+    if request.method == 'GET':
+        all_places = Place.query.all()
+        return jsonify([p.dict for p in all_places]), 200
+    # POST a new review
+    elif request.method == 'POST':
+        response_data = request.get_json()
+        p = Place(
+            name = response_data['name'],
+            address = response_data['address']
+        )
+        db.session.add(p)
+        db.session.commit()
+        return {'success' : 'all good!'}, 200
+
+
+@app.route('/place/<id>', methods=['GET'])
+def get_place(id):
+    # Return a specific place
+    if request.method == 'GET':
+        place = Place.query.filter_by(place_id=id).first()
+        return jsonify(place.dict), 200
+
+# ----------------  Review  ---------------- #
+
+@app.route('/review', methods=['GET', 'POST'])
+def all_reviews():
+    # Return all reviews as a json list of dicts
+    if request.method == 'GET':
+        all_reviews = Review.query.all()
+        return jsonify([r.dict for r in all_reviews]), 200
+    # POST a new review
+    elif request.method == 'POST':
+        response_data = request.get_json()
+        p = Review(
+            person_id = response_data['person_id'],
+            place_id = response_data['place_id'],
+            rating = response_data['rating'],
+            content = response_data['content']
+        )
+        db.session.add(p)
+        db.session.commit()
+        return {'success' : 'all good!'}, 200
+
+@app.route('/review/<id>', methods=['GET'])
+def get_review(id):
+    # Return a specific review
+    if request.method == 'GET':
+        review = Review.query.filter_by(review_id=id).first()
+        return jsonify(review.dict), 200
